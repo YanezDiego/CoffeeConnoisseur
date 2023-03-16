b6b84ffee9f46ca240c5a5baa66520799df16180
Diego Yanez <diegosyanez92@gmail.com>
2023-03-16 15:05:21 -0400

    add info

--- components/banner.js
+++ components/banner.js
@@ -1,5 +1,14 @@
 const Banner = () => {
-    return <div>Banner</div>
+    return (
+      <div>
+        <h1>
+          <span>Coffee</span>
+          <span>Connoisseur</span>
+        </h1>
+        <p>Find new Coffee Shops near by</p>
+        <button>View Stores</button>
+      </div>
+    );
 }
 
 export default Banner
